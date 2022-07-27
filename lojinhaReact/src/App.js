25b1e1a546fed39ebd5b8448aee19851111d8fd0
Isabelli Batista <isabelli.batista@laborit.com.br>
2022-07-27 14:22:25 -0300

    adiciona componente footer

--- lojinhaReact/src/App.js
+++ lojinhaReact/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from './components/Footer/Footer.jsx';
 import Header from './components/Header/Header.jsx'
 
 const App = () => {
   return (
     <>
     <Header />
+    <Footer />
     </>
   );
 }
---
+++ lojinhaReact/src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react'
+import './style.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <section class="footer-menu">
+        <div class="container">
+          <div class="itens-footer">
+            <div class="footer-item-menu">
+              <h3>FOOTER1</h3>
+              <p>Item</p>
+              <p>Item</p>
+              <p>Item</p>
+            </div>
+            <div class="footer-item-menu">
+              <h3>FOOTER1</h3>
+              <p>Item</p>
+              <p>Item</p>
+              <p>Item</p>
+            </div>
+            <div class="footer-item-menu">
+              <h3>FOOTER1</h3>
+              <p>Item</p>
+              <p>Item</p>
+              <p>Item</p>
+            </div>
+            <div class="footer-item-menu">
+              <h3>FOOTER1</h3>
+              <p>Item</p>
+              <p>Item</p>
+              <p>Item</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="footer-logo">
+        <img src="logo-sw.svg" alt="logo com letras s e w" />
+      </section>
+    </footer>
+  );
+}
+export default Footer;
