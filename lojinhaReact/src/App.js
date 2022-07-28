077985556ae8abaad318432e9cf168e743100a23
Isabelli Batista <isabelli.batista@laborit.com.br>
2022-07-28 11:18:51 -0300

    adiciona banner home

--- lojinhaReact/src/App.js
+++ lojinhaReact/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Footer from './components/Footer/Footer.jsx';
-import Header from './components/Header/Header.jsx'
+import Banner from './components/Banner/index.jsx';
+import Footer from './components/Footer/index.jsx';
+import Header from './components/Header/index.jsx'
 
 const App = () => {
   return (
     <>
     <Header />
+    <Banner />
     <Footer />
     </>
   );
---
+++ lojinhaReact/src/components/Banner/index.jsx
@@ -0,0 +1,17 @@
+
+import * as React from 'react'
+import './style.css'
+
+const Banner = () => {
+  return (
+    <main>
+      <section class="section-banner-slide">
+        <div class="container">
+          <h1>NEW COLLECTION</h1>
+          <h2>SPRING/SUMMER2021</h2>
+        </div>
+      </section>
+    </main>
+  );
+}
+export default Banner;
